30256387a209951c05bcf5e23c84a43c0c5fa2be
Julius Hietala <julius.hietala@gmail.com>
2021-11-01 16:01:32 +0200

    load config from json

--- realsense/record.py
+++ realsense/record.py
@@ -15,7 +15,30 @@ from os.path import exists, join
 import shutil
 import json
 from enum import IntEnum
-import inquirer
+import time
+
+class PostProcessor():
+    def __init__(self):
+        self.decimation = rs.decimation_filter()
+        self.spatial = rs.spatial_filter()
+        self.spatial.set_option(rs.option.filter_magnitude, 5)
+        self.spatial.set_option(rs.option.filter_smooth_alpha, 1)
+        self.spatial.set_option(rs.option.filter_smooth_delta, 50)
+        self.spatial.set_option(rs.option.holes_fill, 3)
+        self.hole_filling = rs.hole_filling_filter()
+        self.temporal = rs.temporal_filter()
+        self.depth_to_disparity = rs.disparity_transform(True)
+        self.disparity_to_depth = rs.disparity_transform(False)
+
+    def __call__(self, input_frame):
+        frame = self.decimation.process(input_frame)
+        frame = self.depth_to_disparity.process(frame)
+        frame = self.spatial.process(frame)
+        frame = self.temporal.process(frame)
+        frame = self.disparity_to_depth.process(frame)
+        frame = self.hole_filling.process(frame)
+        return frame
+
 
 
 class Preset(IntEnum):
@@ -27,16 +50,17 @@ class Preset(IntEnum):
     MediumDensity = 5
 
 
-def make_clean_folder(path_folder):
-    if not exists(path_folder):
-        makedirs(path_folder)
-    else:
-        user_input = input("%s not empty. Overwrite? (y/n) : " % path_folder)
-        if user_input.lower() == 'y':
-            shutil.rmtree(path_folder)
+def make_clean_folders(args, path_output, path_depth, path_color, path_colorized_depth):
+    for path_folder in [path_output, path_depth, path_colorized_depth, path_color]:
+        if not exists(path_folder):
             makedirs(path_folder)
         else:
-            exit()
+            user_input = input("%s not empty. Overwrite? (y/n) : " % path_folder)
+            if user_input.lower() == 'y':
+                shutil.rmtree(path_folder)
+                makedirs(path_folder)
+            else:
+                exit()
 
 
 def save_intrinsic_as_json(filename, frame, depth_scale, fps):
@@ -61,6 +85,20 @@ def save_intrinsic_as_json(filename, frame, depth_scale, fps):
             outfile,
             indent=4)
 
+def setup_pipeline_and_config(args, json_config):
+    pipeline = rs.pipeline()
+    config = rs.config()
+    pipeline_wrapper = rs.pipeline_wrapper(pipeline)
+    pipeline_profile = config.resolve(pipeline_wrapper)
+    device = pipeline_profile.get_device()
+    device.hardware_reset()
+    time.sleep(args.reset_sleep)
+    advaced_mode = rs.rs400_advanced_mode(device)
+    advaced_mode.load_json(str(json_config).replace("'", '\"'))
+    return pipeline, config
+
+
+
 
 if __name__ == "__main__":
 
@@ -70,85 +108,34 @@ if __name__ == "__main__":
     parser.add_argument("--out",
                         default='./collected_dataset/',
                         help="set output folder")
+    parser.add_argument("--reset-sleep",
+                        default=5,
+                        help="Time to sleep (seconds) after hardware reset.")
+    parser.add_argument("--config",
+                        default="./rsconfig.json",
+                        help="Time to sleep (seconds) after hardware reset.")
 
     args = parser.parse_args()
-
     path_output = args.out
     path_depth = join(args.out, "depth")
+    path_colorized_depth = join(args.out, "colorized_depth")
     path_color = join(args.out, "color")
-    make_clean_folder(path_output)
-    make_clean_folder(path_depth)
-    make_clean_folder(path_color)
-    pipeline = rs.pipeline()
-    config = rs.config()
-    pipeline_wrapper = rs.pipeline_wrapper(pipeline)
-    pipeline_profile = config.resolve(pipeline_wrapper)
-    device = pipeline_profile.get_device()
+    make_clean_folders(args, path_output, path_depth, path_color, path_colorized_depth)
+    json_config = json.load(open(args.config))
+    pipeline, config = setup_pipeline_and_config(args, json_config)
+    fps = int(json_config['stream-fps'])
+    width, height = int(json_config['stream-width']), int(json_config['stream-height'])
+    config.enable_stream(rs.stream.depth, width, height, rs.format.z16, fps)
+    config.enable_stream(rs.stream.color, width, height, rs.format.bgr8, fps)
 
-    #Loop through available sensors
-    for sensor in device.sensors:
-        if sensor.is_color_sensor():
-            sensor_type = "color"
-        elif sensor.is_depth_sensor():
-            sensor_type = "depth"
-        else:
-            print("Unknown sensor type, skipping.")
-            continue
-
-        options = []
-
-        #Loop through stream profiles
-        for profile in sensor.get_stream_profiles():
-            profile = profile.as_video_stream_profile()
-            #Pick streams with fps at least 30 and proper formats
-            if profile.fps() >= 30:
-                if (sensor_type == "color" and int(profile.format()) == int(rs.format.bgr8)) or (sensor_type == "depth" and int(profile.format() == int(rs.format.z16))):
-                    option = dict(width=profile.width(), height=profile.height(), fps=profile.fps(), format=profile.format())
-                    options.append(option)
-        
-        #Prompt selection of available resolutions/fps in the shell
-        prompt = [
-        inquirer.List('value',
-                        message=f"Choose {sensor_type} sensor stream type",
-                        choices=options
-                    ),
-        ]
-        result = inquirer.prompt(prompt)
-        width = result["value"]["width"]
-        height = result["value"]["height"]
-        format = result["value"]["format"]
-        fps = result["value"]["fps"]
-        stream_type = profile.stream_type()
-        config.enable_stream(stream_type, width, height, format, fps)
-
-    # Start streaming
     profile = pipeline.start(config)
     depth_sensor = profile.get_device().first_depth_sensor()
-    color_sensor = profile.get_device().first_color_sensor()
 
-    #Set sensor options, all available here: https://intelrealsense.github.io/librealsense/python_docs/_generated/pyrealsense2.option.html
-
-    #Set depth sensor options, recommendations: https://dev.intelrealsense.com/docs/d400-series-visual-presets
-    depth_sensor.set_option(rs.option.visual_preset, Preset.HighAccuracy)  # Using preset HighAccuracy for recording
-
-    #Set color sensor options
-    color_sensor.set_option(rs.option.enable_auto_exposure, 0.0) #Disable auto exposure
-    color_sensor.set_option(rs.option.enable_auto_white_balance, 0.0) #Disable auto white balance  
-    color_sensor.set_option(rs.option.white_balance, 3700.0) #White balance, adjust as needed   
-    color_sensor.set_option(rs.option.gain, 64.0) #Gain, adjust as needed  
-    color_sensor.set_option(rs.option.exposure, 953.0) #Exposure, adjust as needed 
-
-    # Getting the depth sensor's depth scale (see rs-align example for explanation)
+    colorizer = rs.colorizer()
+    post_processor = PostProcessor()
+    
     depth_scale = depth_sensor.get_depth_scale()
 
-    # We will not display the background of objects more than
-    #  clipping_distance_in_meters meters away
-    clipping_distance_in_meters = 3  # 3 meter
-    clipping_distance = clipping_distance_in_meters / depth_scale
-
-    # Create an align object
-    # rs.align allows us to perform alignment of depth frames to others frames
-    # The "align_to" is the stream type to which we plan to align depth frames.
     align_to = rs.stream.color
     align = rs.align(align_to)
 
@@ -166,11 +153,17 @@ if __name__ == "__main__":
             aligned_depth_frame = aligned_frames.get_depth_frame()
             color_frame = aligned_frames.get_color_frame()
 
+
             # Validate that both frames are valid
             if not aligned_depth_frame or not color_frame:
                 continue
 
+            aligned_depth_frame = post_processor(aligned_depth_frame)
+
+            colorized_depth_image = np.asanyarray(colorizer.colorize(aligned_depth_frame).get_data())
+            colorized_depth_image = cv2.resize(colorized_depth_image, (width, height), interpolation=cv2.INTER_NEAREST_EXACT)
             depth_image = np.asanyarray(aligned_depth_frame.get_data())
+            depth_image = cv2.resize(depth_image, (width, height), interpolation=cv2.INTER_NEAREST_EXACT)
             color_image = np.asanyarray(color_frame.get_data())
 
             if frame_count == 0:
@@ -181,24 +174,14 @@ if __name__ == "__main__":
                     (path_depth, frame_count), depth_image)
             cv2.imwrite("%s/%06d.jpg" % \
                     (path_color, frame_count), color_image)
+            cv2.imwrite("%s/%06d.jpg" % \
+                    (path_colorized_depth, frame_count), colorized_depth_image)
             print("Saved color + depth image %06d" % frame_count)
             frame_count += 1
 
-            # Remove background - Set pixels further than clipping_distance to grey
-            grey_color = 153
-            #depth image is 1 channel, color is 3 channels
-            depth_image_3d = np.dstack((depth_image, depth_image, depth_image))
-            bg_removed = np.where((depth_image_3d > clipping_distance) | \
-                    (depth_image_3d <= 0), grey_color, color_image)
-
-            # Render images
-            depth_colormap = cv2.applyColorMap(
-                cv2.convertScaleAbs(depth_image, alpha=0.09), cv2.COLORMAP_JET)
-            images = np.hstack((bg_removed, depth_colormap))
-            cv2.namedWindow('Recorder Realsense', cv2.WINDOW_GUI_NORMAL)
-            cv2.imshow('Recorder Realsense', images)
+            cv2.imshow('Color', color_image)
+            cv2.imshow("Depth", colorized_depth_image)
             key = cv2.waitKey(1)
-
             # if 'esc' button pressed, escape loop and exit program
             if key == 27:
                 cv2.destroyAllWindows()
